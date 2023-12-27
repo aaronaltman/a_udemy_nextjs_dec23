195203bf8952e3450649dc495eb420bcc441ba67
aka <aaronkaltman@gmail.com>
2023-12-27 02:18:12 -0500

    portfolio project 1

---
+++ src/components/mainnav1.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function MainNav1() {
+  return (
+    <main>
+      <nav>
+        <ul>
+          <li>hello world</li>
+        </ul>
+      </nav>
+    </main>
+  );
+}
